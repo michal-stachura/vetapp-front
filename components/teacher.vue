c785a5a75cf355a326eb4f356125c07cee8c16c8
Michal Stachura <michal.stachura@santri.pl>
2025-09-29 07:30:25 +0200

    teacher component

--- components/teacher.vue
+++ components/teacher.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-blue-400 p-4 rounded">
     <h4 class="font-bold">Teacher Component</h4>
-    Spinning Kids:
+    <p>Spinning Kids:</p>
     <ul>
       <li>Kid 1</li>
       <li>Kid 2</li>
